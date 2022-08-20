38391c06b157762c3d2a9e326c88626849dd0015
GenkiHirano <genki86web@gmail.com>
2022-08-20 14:16:28 +0900

    refactor: Hello関数をリファクタリング

--- hello.go
+++ hello.go
@@ -2,15 +2,17 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
 func Hello(name string, langage string) string {
 	if name == "" {
 		name = "World"
 	}
 
-	if langage == "Spanish" {
-		return "Hola, " + name
+	if langage == spanish {
+		return spanishHelloPrefix + name
 	}
 
 	return englishHelloPrefix + name
