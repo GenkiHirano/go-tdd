88e76c01a900b986894586d30cd751909bbe8cc4
GenkiHirano <genki86web@gmail.com>
2022-08-20 14:27:18 +0900

    feat: 言語をフランス語にした際は、フランス語で挨拶を返すよう実装

--- hello.go
+++ hello.go
@@ -2,20 +2,29 @@ package main
 
 import "fmt"
 
-const spanish = "Spanish"
-const englishHelloPrefix = "Hello, "
-const spanishHelloPrefix = "Hola, "
+const (
+	spanish            = "Spanish"
+	french             = "French"
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
 
 func Hello(name string, langage string) string {
 	if name == "" {
 		name = "World"
 	}
 
-	if langage == spanish {
-		return spanishHelloPrefix + name
+	prefix := englishHelloPrefix
+
+	switch langage {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
 	}
 
-	return englishHelloPrefix + name
+	return prefix + name
 }
 
 func main() {
--- hello_test.go
+++ hello_test.go
@@ -4,11 +4,11 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	assertCorrectMessage := func(t testing.TB, got, want string) {
-        t.Helper()
-        if got != want {
-            t.Errorf("got %q want %q", got, want)
-        }
-    }
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := Hello("Chris", "")
