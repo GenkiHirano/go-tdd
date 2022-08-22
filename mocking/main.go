77d6bac1c0a684b41e2db2e101664fe2079066bb
GenkiHirano <genki86web@gmail.com>
2022-08-22 16:46:00 +0900

    test: Sleepの順序が正しいか担保するテストを追加

--- mocking/main.go
+++ mocking/main.go
@@ -16,16 +16,33 @@ type Sleeper interface {
 	Sleep()
 }
 
-func Countdown(out io.Writer) {
+func Countdown(out io.Writer, sleeper Sleeper) {
+	// 誤ったコード (デバッグ用)
+	// for i := countdownStart; i > 0; i-- {
+	// 	sleeper.Sleep()
+	// }
+
+	// for i := countdownStart; i > 0; i-- {
+	// 	fmt.Fprintln(out, i)
+	// }
+
+	// 正しいコード
 	for i := countdownStart; i > 0; i-- {
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 		fmt.Fprintln(out, i)
 	}
 
-	time.Sleep(1 * time.Second)
+	sleeper.Sleep()
 	fmt.Fprint(out, finalWord)
 }
 
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
