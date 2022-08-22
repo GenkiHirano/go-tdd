3f7b591ef2be9a371649f1a48718b8f743204186
GenkiHirano <genki86web@gmail.com>
2022-08-22 14:14:12 +0900

    test: Updateで存在しない値・存在する値が更新された時のテストを追加

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -44,13 +44,28 @@ func TestAdd(t *testing.T) {
 }
 
 func TestUpdate(t *testing.T) {
-	word := "test"
-	definition := "this is just a test"
-	dictionary := Dictionary{word: definition}
-	newDefinition := "new definition"
+	t.Run("new word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{}
 
-	dictionary.Update(word, newDefinition)
-	assertDefinition(t, dictionary, word, newDefinition)
+		err := dictionary.Update(word, definition)
+
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, newDefinition)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		newDefinition := "new definition"
+
+		err := dictionary.Update(word, newDefinition)
+
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, newDefinition)
+	})
 }
 
 func assertError(t *testing.T, got, want error) {
