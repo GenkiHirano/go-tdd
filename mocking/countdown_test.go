62c77b2a3c9f2510b11f8570c519858a98739d27
GenkiHirano <genki86web@gmail.com>
2022-08-22 16:51:49 +0900

    test: TestCountdownをリファクタリング

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -6,14 +6,6 @@ import (
 	"testing"
 )
 
-type SpySleeper struct {
-	Calls int
-}
-
-func (s *SpySleeper) Sleep() {
-	s.Calls++
-}
-
 const (
 	sleep = "sleep"
 	write = "write"
@@ -35,9 +27,7 @@ func (s *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
 func TestCountdown(t *testing.T) {
 	t.Run("prints 3 to Go!", func(t *testing.T) {
 		buffer := &bytes.Buffer{}
-		spySleeper := &SpySleeper{}
-
-		Countdown(buffer, spySleeper)
+		Countdown(buffer, &CountdownOperationsSpy{})
 
 		got := buffer.String()
 		want := `3
@@ -47,10 +37,6 @@ Go!`
 
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
-		}
-
-		if spySleeper.Calls != 4 {
-			t.Errorf("not enough calls to sleeper, want 4 got %d", spySleeper.Calls)
 		}
 	})
 
