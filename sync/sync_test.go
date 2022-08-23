418bd9098986f03e1fb40ce24039dde04f3b3e18
GenkiHirano <genki86web@gmail.com>
2022-08-23 19:55:49 +0900

    feat: Counterが並行処理で安全に実行できるように実装

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -36,7 +36,7 @@ func TestCounter(t *testing.T) {
 
 func assertCounter(t *testing.T, got Counter, want int) {
 	t.Helper()
-	if got.Value() != 3 {
-		t.Errorf("got %d want %d", got.Value(), 3)
+	if got.Value() != want {
+		t.Errorf("got %d want %d", got.Value(), want)
 	}
 }
