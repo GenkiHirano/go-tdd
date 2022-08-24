00bf8db92154e18189cdf5ae79c6b645bed03c0a
GenkiHirano <genki86web@gmail.com>
2022-08-24 15:07:01 +0900

    feat: コンテキストを使って、Server関数を実装

--- context/context.go
+++ context/context.go
@@ -2,6 +2,7 @@ package context1
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 )
 
@@ -11,19 +12,7 @@ type Store interface {
 
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// ctx := r.Context()
-
-		// data := make(chan string, 1)
-
-		// go func() {
-		// 	data <- store.Fetch()
-		// }()
-
-		// select {
-		// case d := <-data:
-		// 	fmt.Fprint(w, d)
-		// case <-ctx.Done():
-		// 	store.Cansel()
-		// }
+		data, _ := store.Fetch(r.Context())
+		fmt.Fprint(w, data)
 	}
 }
