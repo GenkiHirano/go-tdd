b2350f57b0d226ab130df6da1c71a5f44ba3ee22
GenkiHirano <genki86web@gmail.com>
2022-08-29 19:48:24 +0900

    feat: ConvertToArabicを最低限で実装し、ファイル整理

--- roman_numerals/numeral_test.go
+++ roman_numerals/numeral_test.go
@@ -2,7 +2,6 @@ package roman_numerals
 
 import (
 	"fmt"
-	"strings"
 	"testing"
 )
 
@@ -63,38 +62,4 @@ func TestConvertingToArabic(t *testing.T) {
 			}
 		})
 	}
-}
-
-type RomanNumeral struct {
-	Value  int
-	Symbol string
-}
-
-var allRomanNumerals = []RomanNumeral{
-	{1000, "M"},
-	{900, "CM"},
-	{500, "D"},
-	{400, "CD"},
-	{100, "C"},
-	{90, "XC"},
-	{50, "L"},
-	{40, "XL"},
-	{10, "X"},
-	{9, "IX"},
-	{5, "V"},
-	{4, "IV"},
-	{1, "I"},
-}
-
-func ConvertToRoman(arabic int) string {
-	var result strings.Builder
-
-	for _, numeral := range allRomanNumerals {
-		for arabic >= numeral.Value {
-			result.WriteString(numeral.Symbol)
-			arabic -= numeral.Value
-		}
-	}
-
-	return result.String()
 }
