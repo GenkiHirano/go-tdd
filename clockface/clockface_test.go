2372b4e44855952e5bb178a2a20639d7ee54e087
GenkiHirano <genki86web@gmail.com>
2022-08-31 19:49:31 +0900

    feat: SecondHand関数を実装

--- clockface/clockface_test.go
+++ clockface/clockface_test.go
@@ -4,16 +4,16 @@ import (
 	"testing"
 	"time"
 
-	"github.com/quii/learn-go-with-tests/math/v1/clockface"
+	"github.com/GenkiHirano/tdd-go.git/clockface"
 )
 
 func TestSecondHandAtMidnight(t *testing.T) {
-    tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
 
-    want := clockface.Point{X: 150, Y: 150 - 90}
-    got := clockface.SecondHand(tm)
+	want := clockface.Point{X: 150, Y: 150 - 90}
+	got := clockface.SecondHand(tm)
 
-    if got != want {
-        t.Errorf("Got %v, wanted %v", got, want)
-    }
+	if got != want {
+		t.Errorf("Got %v, wanted %v", got, want)
+	}
 }
