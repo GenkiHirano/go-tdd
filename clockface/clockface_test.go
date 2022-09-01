38131411ffefdf360b7e107576628a2657b49b4c
GenkiHirano <genki86web@gmail.com>
2022-09-01 23:43:24 +0900

    feat: SecondsInRadians実装

--- clockface/clockface_test.go
+++ clockface/clockface_test.go
@@ -1,30 +1,19 @@
 package clockface_test
 
 import (
+	"math"
 	"testing"
 	"time"
 
 	"github.com/GenkiHirano/tdd-go.git/clockface"
 )
 
-func TestSecondHandAtMidnight(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+func TestSecondsInRadians(t *testing.T) {
+	thirtySeconds := time.Date(312, time.October, 28, 0, 0, 30, 0, time.UTC)
+	want := math.Pi
+	got := clockface.SecondsInRadians(thirtySeconds)
 
-	want := clockface.Point{X: 150, Y: 150 - 90}
-	got := clockface.SecondHand(tm)
-
-	if got != want {
-		t.Errorf("got %v, want %v", got, want)
-	}
-}
-
-func TestSecondHandAt30Seconds(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
-
-	want := clockface.Point{X: 150, Y: 150 + 90}
-	got := clockface.SecondHand(tm)
-
-	if got != want {
-		t.Errorf("got %v, want %v", got, want)
+	if want != got {
+		t.Fatalf("Wanted %v radians, but got %v", want, got)
 	}
 }
